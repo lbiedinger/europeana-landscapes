fdab236d140e9407241f71c520a142eee3040e37
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-08-24 15:21:05 +0200

    use thumbnails

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'rails_12factor', group: :production
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'dotenv-rails', '~> 2.0'
 end
 
 group :development do
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,2 +1,16 @@
+require 'cgi'
+require 'open-uri'
+require 'json'
+
 class Painting < ActiveRecord::Base
+  def europeana_record
+    url = "http://www.europeana.eu/api/v2/record/#{self.europeana_id}.json?wskey=#{ENV["EUROPEANA_API_KEY"]}"
+    JSON.load(open(url))["object"]
+  end
+
+  def thumbnail_url
+    media_uri = europeana_record["aggregations"].first["edmIsShownBy"]
+    thumbnail_url ||= "https://www.europeana.eu/api/v2/thumbnail-by-url.json?size=w200&uri=#{CGI.escape media_uri}&type=IMAGE"
+    thumbnail_url ||= "https://www.europeana.eu/api/v2/thumbnail-by-url.json?size=w200&type=IMAGE"
+  end
 end
