296f08660dd947791abbff9aa1a8227408aa059e
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-08-24 12:00:07 +0200

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160822151222) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "paintings", force: :cascade do |t|
     t.string   "europeana_id"
     t.string   "location"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+paintings = Painting.create([
+    {europeana_id:"2063604/DEN_280_008", location: "Denmark"},
+    {europeana_id:"2063618/FI_280_002", location: "Finland"},
+    {europeana_id:"2063618/FI_280_003", location:"France"},
+    {europeana_id:"2063623/GER_280_005", location:"Germany"},
+    {europeana_id:"2063612/NO_280_007", location:"Norway"},
+    {europeana_id:"2063601/_7BEA127D59D24CD9846E010F46F7954C", location:"Romania"},
+    {europeana_id:"2063604/DEN_280_001", location:"Denmark"},
+    {europeana_id:"2021659/KD2003_0371", location:"Netherlands"}
+])
